6ad13bdf96fa770f279cfded93d5b73e8620c654
Vipul Tyagi <tyagivipul11.vt@gmail.com>
2024-10-04 21:15:12 +0200

    Data Transformation testing completed

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import os
 import sys
 from src.logging.logger import logging 
 from src.exception.exception import NetworkSecurityException
-from src.entity.config_entity import MLOpsPipelineConfig, DataIngestionConfig, DataValidationConfig
+from src.entity.config_entity import MLOpsPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from src.entity.artifact_entity import DataIngestionArtifact
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
+from src.components.data_transformation import DataTransformation
 from dotenv import load_dotenv
 
 # Loading the environment variables
@@ -34,6 +35,14 @@ if __name__ == "__main__":
         logging.info("Data Validation process completed successfully")
         print("Data Validation process completed successfully")
 
+        # Data Transformation Process
+        data_transformation_config = DataTransformationConfig(mlops_pipeline_config)
+        data_tranformation = DataTransformation(validation_data_artifact, data_transformation_config)
+        logging.info("Data Transformation process started")
+        print("Data Transformation process started")
+        data_transformation_artifact = data_tranformation.initiate_data_transformation()
+        logging.info("Data Transformation process completed successfully")
+        print("Data Transformation process completed successfully")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
