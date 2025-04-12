4aac81b8e26f401d152e64956ad07c7690b741a4
Vipul Tyagi <tyagivipul11.vt@gmail.com>
2025-04-12 23:36:37 +0200

    steps to setup aws EC2 and a test main.py file

---
+++ main.py
@@ -0,0 +1,50 @@
+from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
+from src.components.data_transformation import DataTransformation
+from src.exception.exception import NetworkSecurityException
+from src.logging.logger import logging
+from src.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from src.entity.config_entity import MLOpsPipelineConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.entity.config_entity import ModelTrainerConfig
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mongo_db_url=os.getenv("MONGO_DB_URL") ## use it in each file to load it
+
+if __name__=='__main__':
+    try:
+        trainingpipelineconfig=MLOpsPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(dataingestionconfig, mongo_db_url=mongo_db_url)
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
+        print(dataingestionartifact)
+        
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+        
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
+
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training artifact created")
+        
+    except Exception as e:
+           raise NetworkSecurityException(e,sys)
