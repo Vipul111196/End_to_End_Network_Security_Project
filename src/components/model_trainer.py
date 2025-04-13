2908b6640df4086fe18b80021aa319491885f2c5
Vipul Tyagi <tyagivipul11.vt@gmail.com>
2025-04-13 13:50:02 +0200

    deactivated dagshub

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -24,7 +24,6 @@ load_dotenv()
 
 # dagshub.init(repo_owner="Vipul111196", repo_name="End_to_End_Network_Security_Project", mlflow=True)
 
-
 # os.environ["MLFLOW_TRACKING_URI"]= "http://127.0.0.1:5000" # Running Mlflow server on localhost
 
 class ModelTrainer:
